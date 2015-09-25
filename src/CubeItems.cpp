6c502bec692daac02d3f4dfe64022a454a02ea05
g-bunny <astarrynite@gmail.com>
2015-09-25 18:11:48 -0400

    class for cube items added. not doing anything with it yet

---
+++ src/CubeItems.cpp
@@ -0,0 +1,9 @@
+//
+//  CubeItems.cpp
+//  spaceTimeAndBunnies
+//
+//  Created by Miri Park on 9/23/15.
+//
+//
+
+#include "CubeItems.h"
---
+++ src/CubeItems.h
@@ -0,0 +1,17 @@
+//
+//  CubeItems.h
+//  spaceTimeAndBunnies
+//
+//  Created by Miri Park on 9/23/15.
+//
+//
+
+#include "ofMain.h"
+//#include "Frame.hpp"
+
+class CubeItems {
+public:
+    CubeItems();
+    void update();
+    void display();
+};
