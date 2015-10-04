52dbdebc1c4e9178ea91a8b61e1b01d881fec9be
g-bunny <astarrynite@gmail.com>
2015-10-04 14:57:46 -0400

    moving behaviors to chapters class. creating a chapter1 object

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,255 +1,67 @@
 #include "ofApp.h"
-int height = 20;
-int width = 30;
-int insideLength = 15;
 
-//--------------------------------------------------------------
+
 void ofApp::setup(){
-    const ofColor lavender = ofColor(176,183,255);
-    const ofColor blue = ofColor(178,255,233);
-    const ofColor red = ofColor(242,105,105);
-
-    this->firstFrame = new Frame(lavender, blue, blue, 300, 300, 100, 275, 250, 1);
-    this->secondFrame = new Frame(lavender, lavender, blue, 300, 300, 300, 687.5,250, 1);
-    this->thirdFrame = new Frame(lavender, blue, red, 300, 300, 100, 1100, 250, 1);
-    this->fourthFrame = new Frame(lavender, red, red, 1125, 300, 100, 687.5, 625, 2);
-    this->one = new Characters(200,248,0,1);
-    this->zero = new Characters(1070,570,0,2);
-    this->timer = new Timer();
-    this->brickChimney = new Item(2, 1050, 296);
-    this->door = new Item(2,1030,540);
-    this->clock = new Item(3, 690,200);
-//    talk.loadSound("sounds/chimes.wav");
-    knock.loadSound("sounds/knock.wav");
-    chimes.loadSound("sounds/chimes.wav");
-    talk.setVolume(0.75f);
-    knock.setVolume(0.75f);
-    chimes.setVolume(0.75f);
-//    talk.setMultiPlay(true);
-    knock.setMultiPlay(false);
-    chimes.setMultiPlay(false);
-    ofBackground(255);
+//    this->chapter1 = new Chapter1();
+    this->chapter7 = new Chapter7(4, 300);
+    this->chapter1 = new Chapter7(1, 500);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    ofCamera();
-    ofSoundUpdate();
+    if (currentChapter == 2){
+        chapter7->update();
+    } else if (currentChapter == 1){
+        chapter1->update();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    firstFrame->display();
-    
-    secondFrame->display();
-    thirdFrame->display();
-    fourthFrame->display();
-    
-    firstFrame->draw();
-
-    secondFrame->draw();
-    thirdFrame->draw();
-    fourthFrame->draw();
-//    secondFrame->lines();
-//    zero->display();
-    if (door->interactionInitiated == true){
-        door->drawOpen();
+    if (currentChapter == 2){
+        chapter7->draw();
+    } else if (currentChapter == 1){
+        chapter1->draw();
     }
-//    if (int(clock->rotationS)%360 < 15 && int(clock->rotationS)%360 > 0 && door->doorTime == true){
-//        if (int(clock->rotationM)%360 < 15 && int(clock->rotationM)%360 > 0 && door->doorTime == true){
-//            if (int(clock->rotationH)%360 < 15 && int(clock->rotationH)%360 > 0 && door->doorTime == true){
-//                if(door->interactionInitiated){
-//                                showZero = true;
-//                }
-//            }
-//        }
-//    }
-    //////TOGGLE FOR EASY MODE
-    if (int(clock->rotationS)%360 < 15 && int(clock->rotationS)%360 > 0 && door->doorTime == true){
-                    showZero = true;
-    }
-
-    
-    if(showZero == true){
-        zero->display();
-        zero->move();
-        chimes.play();
-
-    }
-
-    brickChimney->drawChimney();
-    if (door->interactionInitiated == false){
-        door->drawClosed();
-    }
-    one->display();
-    one->move();
-    one->jump();
-
-    if (one->moveRight == true){
-        clock->movementFactorS += .5;
-
-    }
-    if (one->moveLeft == true){
-        clock->movementFactorS -= .3;
-    }
-    
-    if (one->location.x < 100 || (one->location.x>400 && one->location.x < 500) || (one->location.x > 800 && one->location.x < 900) || (one->location.x > 1200)){
-        clock->movementFactorH += .1;
-    }
-    
-    if (one->location.y < one->startingYpos){
-        clock->movementFactorM += .2;
-    }
-    if(secondFrame->yRotateState ==1){
-        ofSetColor(178,255,233);
-        clock->drawHours();
-        ofSetColor(255);
-        clock->drawMinutes();
-        ofSetColor(242,105,105);
-        clock->drawSeconds();
-    }
-    
-    if (one->location.x > 1030 && one->location.x < 1130 && one->location.y <215 && one->location.y > 190){
-        downTheChimney = true;
-    }
-    
-    if(downTheChimney == true){
-        one->transported = true;
-    }
-    
-    if(one->location.x >= 1010 && one->location.x <= 1150 && one->location.y == 625){
-        appropriateDist = true;
-    } else {
-        appropriateDist = false;
-    }
-    
-    if(int(clock->rotationS)%360 < 15 && int(clock->rotationS)%360 > 0){
-        if (door->interactionInitiated == false){
-            door->doorTime = true;
-            knock.play();
-        } else {
-            door->doorTime = false;
-        }
-    } else {
-        door->doorTime = false;
-    }
-
-
-//    cout << "mins " << clock->mins << endl;
-    cout << "rotationS : " << clock->rotationS << endl;
-    cout << "location.y? " << one->location.y << endl;
-    cout << "location.x? " << one->location.x << endl;
-
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-    if (key == OF_KEY_RIGHT){
-        one->moveRight = true;
-    }
-    if (key == OF_KEY_LEFT){
-        one->moveLeft = true;
-    }
-    if (key == OF_KEY_UP){
-        one->moveUp = true;
-    }
-    if (key == OF_KEY_DOWN){
-        one->moveDown = true;
-    }
-    if (key == OF_KEY_RIGHT_SHIFT){
-        one->jumped = true;
-    }
-    if (key == OF_KEY_LEFT_SHIFT){
-        if (appropriateDist == true){
-            if (door->interactionInitiated == false){
-                door->interactionInitiated = true;
-            } else {
-                door->interactionInitiated = false;
-            }
-        } else {
-            door->interactionInitiated = false;
-        }
-    }
-
+    chapter7->keyPressed(key);
+    chapter1->keyPressed(key);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-    if (key == 'd'){
-        if (one->location.x > 100 && one->location.x<400){
-            firstFrame->flipRight = true;
-        } else if (one->location.x > 500 && one->location.x< 800){
-            secondFrame->flipRight = true;
-        } else if (one->location.x > 900 && one->location.x < 1200){
-            thirdFrame->flipRight = true;
-        }
-    }
-    if (key == 'a'){
-        if (one->location.x > 100 && one->location.x<400){
-            firstFrame->flipLeft = true;
-        } else if (one->location.x > 500 && one->location.x< 800){
-            secondFrame->flipLeft = true;
-        } else if (one->location.x > 900 && one->location.x < 1200){
-            thirdFrame->flipLeft = true;
-        }
-    }
-    if (key == 'w'){
-        if (one->location.x > 100 && one->location.x<400){
-            firstFrame->flipUp = true;
-        } else if (one->location.x > 500 && one->location.x< 800){
-            secondFrame->flipUp = true;
-        } else if (one->location.x > 900 && one->location.x < 1200){
-            thirdFrame->flipUp = true;
-        }
-    }
-    if (key == 's'){
-        if (one->location.x > 100 && one->location.x<400){
-            firstFrame->flipDown = true;
-        } else if (one->location.x > 500 && one->location.x< 800){
-            secondFrame->flipDown = true;
-        } else if (one->location.x > 900 && one->location.x < 1200){
-            thirdFrame->flipDown = true;
-        }
-    }
-    if (key == OF_KEY_RIGHT){
-        one->moveRight = false;
-    }
-    if (key == OF_KEY_LEFT){
-        one->moveLeft = false;
-    }
-    if (key == OF_KEY_UP){
-        one->moveUp = false;
-    }
-    if (key == OF_KEY_DOWN){
-        one->moveDown = false;
-    }
+    chapter1->keyReleased(key);
+    chapter7->keyReleased(key);
 }
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-}
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-}
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-//    if (x < ofGetWindowWidth()/2){
-//        talk.play();
-//        talk.setSpeed(0.1f)
-//    }
-//    if (x > ofGetWindowWidth()/2 && x < ofGetWindowWidth()){
-//        door->interactionInitiated = true;
-//    }
-}
-//--------------------------------------------------------------
+////--------------------------------------------------------------
+//void ofApp::mouseMoved(int x, int y ){
+//}
+////--------------------------------------------------------------
+//void ofApp::mouseDragged(int x, int y, int button){
+//}
+////--------------------------------------------------------------
+//void ofApp::mousePressed(int x, int y, int button){
+////    if (x < ofGetWindowWidth()/2){
+////        talk.play();
+////        talk.setSpeed(0.1f)
+////    }
+////    if (x > ofGetWindowWidth()/2 && x < ofGetWindowWidth()){
+////        door->interactionInitiated = true;
+////    }
+//}
+////--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
+    currentChapter+=1;
 }
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-}
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-}
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-}
+////--------------------------------------------------------------
+//void ofApp::windowResized(int w, int h){
+//}
+////--------------------------------------------------------------
+//void ofApp::gotMessage(ofMessage msg){
+//}
+////--------------------------------------------------------------
+//void ofApp::dragEvent(ofDragInfo dragInfo){ 
+//}
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,55 +1,32 @@
 #pragma once
 
 #include "ofMain.h"
-#include "Frame.hpp"
-#include "Characters.hpp"
-//#include "Timer.h"
-#include "Item.h"
-#include "cubeItems.h"
-#define HOURLENGTH 4
-#define MINLENGTH 5
-#define SECLENGTH 6
+#include "Chapter7.h"
 
 class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
 		void update();
-		void draw();
-
+        void draw();
+    
+    Chapter7 * chapter7;
+    Chapter7 * chapter1;
+//
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
+    int currentChapter = 1;
+    
+    int widthThirds = 300;
+    int widthHalves;
+    int widthFull = 1125;
+    int heightHalf = 300;
+    int heightFull = 700;
+//		void mouseMoved(int x, int y );
+//		void mouseDragged(int x, int y, int button);
+//		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-    Frame * firstFrame;
-    Frame * secondFrame;
-    Frame * thirdFrame;
-    Frame * fourthFrame;
-    Frame * hourFrames[HOURLENGTH];
-    Frame * minFrames[MINLENGTH];
-    Frame * secFrames[SECLENGTH];
-    Characters * one;
-    Characters * zero;
-    Timer * timer;
-    ofSoundPlayer talk;
-    ofSoundPlayer knock;
-    ofSoundPlayer chimes;
-    Item * door;
-    Item * brickChimney;
-    Item * clock;
-    
-    float hourIncrement = 0.5;
-    float minIncrement = 1;
-    float secIncrement = 6;
-    
-    bool downTheChimney = false;
-    bool appropriateDist = false;
-    
-    bool showZero = false;
-
+//		void windowResized(int w, int h);
+//		void dragEvent(ofDragInfo dragInfo);
+//		void gotMessage(ofMessage msg);
 };
